81187d07deee3faf4ff3516dc81843c3992429eb
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2018-01-15 13:42:23 +0700

    Project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-websocket "0.1.0-SNAPSHOT"
   :description "Jetty-free Clojure websocket client"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/brainoutsource/clojure-websocket"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
